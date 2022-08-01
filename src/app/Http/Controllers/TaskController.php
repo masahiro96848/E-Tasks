c86c76a415d96d5bad43d0ac0cfff8c7eb128c52
masahiro96848 <feetwcpic9436@gmail.com>
2022-08-02 08:34:57 +0900

    APIでタスク一覧の表示。ide-helperを追加

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+
+    /**
+     * Task一覧
+     * 
+     * @return \Illuminate\Support\Collection
+     */
+
+    public function index()
+    {
+        return Task::orderByDesc('id')->get();
+    }
 }
