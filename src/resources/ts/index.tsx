2437d53bff2c6d7e08f975edce898ea5b58e02b1
masahiro96848 <feetwcpic9436@gmail.com>
2022-09-03 12:06:37 +0900

    タスク一覧ページを表示

--- src/resources/ts/index.tsx
+++ src/resources/ts/index.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from './App'
 
-const App = () => {
-    return (
-        <h1>Laravel SPA</h1>
-    )
-}
+
 
 ReactDOM.render(
     <React.StrictMode>
